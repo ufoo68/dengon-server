2fc8dc75cc8bc5b269e6fb0cd8682269a7a1895c
Glitch (flavio-websockets-server-example) <none>
2020-08-01 12:40:41 +0000

    🍥🌈 Checkpoint ./server.js:555807/264

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var io = require('socket.io').listen(3000);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app)
+const PORT = 3000
+
+const io = require('socket.io').listen(http);
 io.sockets.on('connection', function (socket) {
 　socket.on('message',function(msg){
         console.log('message: ' + msg);
         io.emit('message', msg);
     })
+})
+
+http.listen(PORT, function(){
+    console.log('server listening. Port:' + PORT);
 })
