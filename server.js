5e67baab9975aa7349c1547f1cd65975880a1702
Glitch (flavio-websockets-server-example) <none>
2020-08-01 12:29:41 +0000

    ⚾️👢 Checkpoint ./server.js:555807/156

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(3000);
 io.sockets.on('connection', function (socket) {
-　
+　socket.on('message',function(msg){
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    })
 })
