d67c5a7d7f04937ff506a7af7891e554a5f330c5
Glitch (flavio-websockets-server-example) <none>
2020-08-01 10:00:15 +0000

    🐇🍉 Checkpoint ./server.js:555807/21

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received message => ${message}`)
+    ws.send(message)
   })
   ws.send('ho!')
 })
